b4b12852bf28f27dea3813c86272d0eda21b0413
singhragini701 <42149188+singhragini701@users.noreply.github.com>
2021-03-16 19:35:41 +0530

    Add files via upload

---
+++ app/edit-phone.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditPhoneComponent } from './edit-phone.component';
+
+describe('EditPhoneComponent', () => {
+  let component: EditPhoneComponent;
+  let fixture: ComponentFixture<EditPhoneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditPhoneComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditPhoneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
